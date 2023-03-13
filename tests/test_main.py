e151e239be45a10f5cf347ddf964ecedd867379e
uwagaDUDE <puvir00@gmail.com>
2023-03-13 11:45:00 +0300

    Wrong video id error

---
+++ tests/test_main.py
@@ -0,0 +1,10 @@
+import main
+from googleapiclient.discovery import build
+def test_main():
+    youtube = build('youtube', 'v3', developerKey='AIzaSyDpe1BX2Gpeq38aHN5dS7A5oeUciTTAUTE')
+    channel = youtube.channels().list(id='UCwEthvsKuX9ZaqvIaGfG3RQ', part='snippet,statistics').execute()
+    channel1 = main.UserInformation(channel, 'UCPx7nkXKVp7iOnxANvMm4HQ')
+    assert str(channel1) == 'YouTube-Канал: Solid'
+    assert str(main.PLVideo('PL7Ntiz7eTKwrqmApjln9u4ItzhDLRtPuD', '9lO06Zxhu88')) == ('Название видео: Как устроена IT-столица мира '
+                                                                                      '/ Russian Silicon Valley (English subs) \n'
+                                                                                      'Плейлист: Литература')
